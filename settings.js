903dc75880b79ef905a74aab4d11574957ee9f36
Kevin-Tsh <158796604+Kevin-Tsh@users.noreply.github.com>
2024-08-29 22:41:18 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,9 +9,9 @@ global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
 global.location = "Pakistan, Karachi, Lahore" //ur location
 
 //new
-global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
+global.botname = '© 𝐓𝐒𝐇_𝐁𝐔𝐆_𝐌𝐃' //ur bot name
+global.ownernumber = ['243891716833'] //ur owner number, dont add more than one
+global.ownername = '© 𝐌𝐑.𝐊𝐄𝐕𝐈𝐍 𝐓𝐒𝐇' //ur owner name
 global.websitex = "https://youtu.be/tLq8_qOOGFI"
 global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
 global.themeemoji = '🪀'
@@ -19,9 +19,9 @@ global.wm = "Xeon Bot Inc."
 global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
 global.packname = "Sticker By"
 global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.creator = "243891716833@s.whatsapp.net"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["243891716833"] // Premium User
 
 //channel id
 global.xchannel = {
